1bd97935b5f8a4f24c7d9e801980d99b8896d17d
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-08-19 13:15:29 -0500

    heatmap of missing values

--- preprocessing.py
+++ preprocessing.py
@@ -2,11 +2,26 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import missingno as msno  # Optional: for visualizing missing data
+import matplotlib.pyplot as plt
 
 
 # Load your dataset into a pandas DataFrame
 df = pd.read_csv('loan-train.csv')  # Replace 'your_dataset.csv' with your actual file path
+# = pd.read_csv('minitest.csv')  # Replace 'your_dataset.csv' with your actual file path
 
 # Display the first few rows of the dataset
-print(df.head())
+#print(df.head())
 
+# Visualize missing data (optional)
+msno.matrix(df)
+msno.heatmap(df)
+plt.show()
+
+# Drop rows with missing values
+df_cleaned = df.dropna()
+msno.matrix(df_cleaned)
+msno.heatmap(df_cleaned)
+plt.show()
+
+# Or, fill missing values with the mean
+#df_filled = df.fillna(df.mean())
