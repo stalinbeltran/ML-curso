f8f0b2a5b24db3f7ae8ac251f0261c58aaf9bd6a
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-08-20 17:56:08 -0500

    .

--- pandasTesting.py
+++ pandasTesting.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy import stats
 
 df = pd.read_csv('loan-train.csv')
-print(df)
-
-
+print(df['Credit_History'])
+print(df.dtypes)
+print(df[ ['Credit_History', 'Education'] ])
+print(df['Education'].replace({'Graduate': 1, 'Not Graduate': 0}))
