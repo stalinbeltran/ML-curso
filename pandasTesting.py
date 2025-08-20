e013faf1ff17d4fa6ae2107c7ba8706d10bd90f8
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-08-20 18:43:37 -0500

    map for each value in dataframe

--- pandasTesting.py
+++ pandasTesting.py
@@ -3,19 +3,23 @@ import numpy as np
 from scipy import stats
 
 df = pd.read_csv('loan-train.csv')
-print(df['Credit_History'])
+print(df.head())
 print(df.dtypes)
 print(df[ ['Credit_History', 'Education'] ])
 print(df['Education'].replace({'Graduate': 1, 'Not Graduate': 0}))
 
-def booleanToInt(row):
-    print('booleanToInt row: ')
-    print(row)
-    return 1 if row.all() else 0
-    
-print(df.apply(booleanToInt, axis = 1))
+def booleanToInt(val):
+    try:
+        if val.lower() == 'yes' or val.lower() == 'y':
+            return 1
+        elif val.lower() == 'no' or val.lower() == 'n':
+            return 0
+        else:
+            return val
+    except:
+        return val
 
 
-
+print(df.map(booleanToInt))
 
 df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
