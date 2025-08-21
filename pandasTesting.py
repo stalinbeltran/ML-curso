6b41f59316e4156a3ade1a2af662283bca12e759
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-08-20 20:15:59 -0500

    lambda for map

--- pandasTesting.py
+++ pandasTesting.py
@@ -2,6 +2,17 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 
+df = pd.DataFrame({
+    'A': [1, 2, 3],
+    'B': [4, 5, 6],
+    'columna': [45,67, 989]
+    }, index = ['primero', 'segundo', 'tercero'])
+print(df)
+print(df.apply(np.mean, axis = 0))
+print(df.loc['segundo'])    #second row
+print(df.iloc[2])       #third row
+
+
 df = pd.read_csv('loan-train.csv')
 print(df.head())
 print(df.dtypes)
@@ -24,14 +35,5 @@ print(df.map(booleanToInt))
 print(df['Married'].apply(booleanToInt))
 df['Married'] = df['Married'].apply(booleanToInt)
 print(df)
-
-
-df = pd.DataFrame({
-    'A': [1, 2, 3],
-    'B': [4, 5, 6],
-    'columna': [45,67, 989]
-    }, index = ['primero', 'segundo', 'tercero'])
-print(df)
-print(df.apply(np.mean, axis = 0))
-print(df.loc['segundo'])
-print(df.iloc[2])
+print(df.map(lambda x: 1 if x == 'Y' else x))
+print(df.map(lambda x: 0 if x == 'N' else x))
